7809c2bc3f103650e425e999b2fe8cbad55a0460
ubuntu <ubuntu@vm172-31-48-10.ksc.com>
2017-08-03 09:28:25 +0800

    mongo +  http + rpc demo

--- app.js
+++ app.js
@@ -1,13 +1,77 @@
-'use strict';
-
 var MobicoinRPC = require('bitcoind-rpc');
+var express = require("express");
+var app = express();
+
+var mongo = require('mongodb').MongoClient;
+var DB_CONN_STR = 'mongodb://localhost:27017/ico_addresses';
+
+var insertData = function(db,callback) {
+	var collection = db.collection('site');
+	var data = [
+		{
+			'TcashAddr':"dkjflkasjoicjzlfjlkaksdjfsjlajfdkas",
+			'BitcoinAddr':"fjddkslaldkjfkdsajflkdsjalfkjsldad"
+		},{
+			'TcashAddr':'fdjksaldkfjldsajdkfldsjaflfdasfdsa',
+			'BitcoinAddr':'fjdklsakdfjlkxzcjkwaeradfddsajfdka'
+		}
+
+	];
+	collection.insert(data,function(err, result){
+		if(err){
+			console.log("ERR:"+err);
+			return;
+		}
+		callback(result);
+	});
+};
+
+mongo.connect(DB_CONN_STR, function(err,db){
+	console.log("连接成功！");
+    	insertData(db, function(result) {
+        	console.log(result);
+        	db.close();
+    	});
+});
 
 var mrpc = new MobicoinRPC({
 	protocol:"http",
-	host:'120.92.44.149',
-	port:8332,
+	host:'127.0.0.1',
+	port:'8332',
 	user:'bitcoin',
 	pass:'local321',
 });
 
-mrpc.getAccountAddress("",);
+mrpc.getAccountAddress("",function (err, ret) {
+	console.log(err);
+	console.log(ret);
+	return;
+});
+
+//获取新地址
+mrpc.getNewAddress(function (err, ret) {
+        console.log(err);
+        console.log(ret);
+        return;
+});
+
+//获取某地址的金币数量
+mrpc.getReceivedByAddress("Jf5HyhCX3XFVshHAX7rGrtqCaHYuntchHb",function (err, ret) {
+        console.log(err);
+        console.log(ret);
+        return;
+});
+
+//http.createServer(function (req , res){
+//	res.writeHead(200,{'Content-Type':'text/plain'});
+//	res.end('Hello World!\n');
+//}).listen(8080);
+//console.log("Server is listening on 8080");
+console.log("getBtcAddrByIcoCoinAddr");
+app.get('/getBtcAddrByIcoCoinAddr',function(req,res){
+	console.log(req.query.addr);
+	res.send("few79801uifuhisaufdhlisaduhfduoaidcoa");
+});
+
+app.listen(8080);
+console.log("Listening on port 8080");
